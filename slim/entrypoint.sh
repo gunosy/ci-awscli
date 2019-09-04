39e0893d8a63d06fa78989d1729e2e79cf2707e2
koid <yukinori.koide@gunosy.com>
2019-09-04 16:23:43 +0900

    add entrypoint for assume-role

---
+++ slim/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [[ -n "${ASSUME_ROLE_ARN}" ]]; then
+    CREDENTIALS=$(aws sts assume-role --role-arn ${ASSUME_ROLE_ARN} --role-session-name ${ASSUME_ROLE_SESSION_NAME:-ci-awscli})
+    export AWS_SECRET_ACCESS_KEY=$(echo ${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey') && \
+    export AWS_SESSION_TOKEN=$(echo ${CREDENTIALS} | jq -r '.Credentials.SessionToken') && \
+    export AWS_ACCESS_KEY_ID=$(echo ${CREDENTIALS} | jq -r '.Credentials.AccessKeyId')
+fi
+
+exec "$@"
